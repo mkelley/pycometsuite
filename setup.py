c37399f5d5507ff47088bf44bea67c636623ac66
Michael S. P. Kelley <msk@astro.umd.edu>
2023-11-16 12:52:28 -0500

    Also install data files.

--- setup.py
+++ setup.py
@@ -4,19 +4,25 @@ from setuptools import setup, Extension, find_packages
 from Cython.Build import cythonize
 
 if __name__ == "__main__":
-    setup(name='cometsuite',
-          version='1.0.0',
-          description='Comet dust dynamics.',
-          author="Michael S. P. Kelley",
-          author_email="msk@astro.umd.edu",
-          packages=find_packages(),
-          scripts=glob('scripts/*'),
-          data_files=[('cometsuite/data', glob('cometsuite/data/*dat'))],
-          ext_modules=cythonize([
-              'cometsuite/integrator/prop2b.pyx',
-              Extension('cometsuite.integrator.bsint',
-                        ['cometsuite/integrator/bsint.pyx'],
-                        libraries=['gsl', 'gslcblas'])
-          ]),
-          license='BSD',
-          )
+    setup(
+        name="cometsuite",
+        version="1.0.0",
+        description="Comet dust dynamics.",
+        author="Michael S. P. Kelley",
+        author_email="msk@astro.umd.edu",
+        packages=find_packages(),
+        scripts=glob("scripts/*"),
+        data_files=[("cometsuite/data", glob("cometsuite/data/*dat"))],
+        include_package_data=True,
+        ext_modules=cythonize(
+            [
+                "cometsuite/integrator/prop2b.pyx",
+                Extension(
+                    "cometsuite.integrator.bsint",
+                    ["cometsuite/integrator/bsint.pyx"],
+                    libraries=["gsl", "gslcblas"],
+                ),
+            ]
+        ),
+        license="BSD",
+    )
