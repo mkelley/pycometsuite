24d8fa7e45f1690bde7559d76041251b6c9d707b
Michael S. P. Kelley <msk@astro.umd.edu>
2023-11-27 15:38:16 -0500

    clean up comments from setup.py

--- setup.py
+++ setup.py
@@ -15,25 +15,3 @@ ext = [
 ]
 
 setup(ext_modules=ext)
-
-# from glob import glob
-# from setuptools import setup, Extension, find_packages
-# from Cython.Build import cythonize
-
-# if __name__ == "__main__":
-#     setup(name='cometsuite',
-#           version='1.0.0',
-#           description='Comet dust dynamics.',
-#           author="Michael S. P. Kelley",
-#           author_email="msk@astro.umd.edu",
-#           packages=find_packages(),
-#           scripts=glob('scripts/*'),
-#           data_files=[('cometsuite/data', glob('cometsuite/data/*dat'))],
-#           ext_modules=cythonize([
-#               'cometsuite/integrator/prop2b.pyx',
-#               Extension('cometsuite.integrator.bsint',
-#                         ['cometsuite/integrator/bsint.pyx'],
-#                         libraries=['gsl', 'gslcblas'])
-#           ]),
-#           license='BSD',
-#           )
