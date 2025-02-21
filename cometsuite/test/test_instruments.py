7633ca6197223dd3abf936e82e7d526468b0f589
Michael S. P. Kelley <msk@astro.umd.edu>
2025-02-21 09:49:23 -0500

    Make test aperture large enough for entire simulation.

--- cometsuite/test/test_instruments.py
+++ cometsuite/test/test_instruments.py
@@ -1,3 +1,4 @@
+import numpy as np
 import astropy.units as u
 from .. import instruments
 from . import sim_radius_uniform
@@ -9,6 +10,6 @@ class TestPhotometer:
         assert inst.data.shape == ()
 
     def test_integrate(self, sim_radius_uniform):
-        inst = instruments.Photometer(5 * u.arcsec)
-        inst.integrate(sim)
+        inst = instruments.Photometer(3 * u.rad)
+        inst.integrate(sim_radius_uniform)
         assert inst.data == 2000
