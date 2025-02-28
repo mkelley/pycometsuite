25827530493ce73a3613e26c17681f57a44b1b13
Michael S. P. Kelley <msk@astro.umd.edu>
2025-02-28 12:16:04 -0500

    Finish example scattered light test.

--- tests/scattered-light.py
+++ tests/scattered-light.py
@@ -38,34 +38,39 @@ w = np.r_[0.95, 1.0, 1.05] * wave * u.um
 S = sun(w, unit="W/(m2 um)")[1]  # at 1 AU
 
 psd = cs.scalers.PSD_PowerLaw(-3.5) * cs.scalers.PSD_RemoveLogBias()
-light = cs.scalers.ScatteredLight(wave, Ap=0.05)
+light = cs.scalers.ScatteredLight(wave, Ap=0.0419)
 
-# calibrate to total mass of 1e6 kg
+# calibrate to total mass of 1.6e6 kg
 M_sim = (psd.scale(sim) * sim.m / 1e3).sum()  # kg
 mass_cal = 1.6e6 / M_sim * psd.scale(sim)
 sigma = mass_cal * np.pi * (1e-6 * sim.radius) ** 2  # m2
 
 Fsim = mass_cal * light.scale(sim)
-G = np.pi / 0.05 * Fsim.sum() * rh**2 / S.value * sim.Delta.mean() ** 2
+G = np.pi / 0.0419 * Fsim.sum() * rh**2 / S.value * sim.Delta.mean() ** 2
 
 # calibrate to total cross sectional area of 118 km2 = 118e6 m2
 G_sim = (psd.scale(sim) * sim.cs).sum() / 1e4  # m2
 area_cal = 118e6 / G_sim * psd.scale(sim)
 M = (area_cal * sim.m).sum() / 1e3  # kg
 
-# sigma = 118 * 1e6
-F = 0.05 / np.pi * sigma * S / rh**2 / (1e3 * Delta) ** 2
+F = 0.0419 / np.pi * sigma * S / rh**2 / (1e3 * Delta) ** 2
 k = wave / 2 / np.pi
 x = sim.radius / k
 i = x < 1
 F[i] *= (sim.radius / k)[i] ** 4
 
+# Kelley et al. measured 9.06 mag
+# This does not include phase correction: 0.938 mag --> 8.12
 m = F.sum().to(u.ABmag, u.spectral_density(wave * u.um))
+assert np.isclose(m.value, 8.12, atol=0.05)
+
+# calculate with the scaler
+m = (Fsim.sum() * u.W / u.m**2 / u.um).to(u.ABmag, u.spectral_density(wave * u.um))
+assert np.isclose(m.value, 8.12, atol=0.05)
 
 i = np.argsort(sim.radius)
 
 plt.clf()
-# plt.plot(sim.radius[i], x[i])
 plt.plot(sim.radius[i], F[i])
 plt.plot(sim.radius[i], Fsim[i])
 plt.xscale("log")
